bf80ba40e3de6366ae6e2896e476ac5b926fd7a3
Linxusthuis <140453732+Linxusthuis@users.noreply.github.com>
2024-06-19 17:21:14 -0300

    asdf

--- js.js
+++ js.js
@@ -6,8 +6,8 @@ function myFunction() {
   
   // Close the dropdown if the user clicks outside of it
   window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("myDropdown");
+    if (!event.target.matches('.myDropdown')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
       var i;
       for (i = 0; i < dropdowns.length; i++) {
         var openDropdown = dropdowns[i];
