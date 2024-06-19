a2d377ca01da1c4b025d6576bc6686dcdfd83732
Linxusthuis <140453732+Linxusthuis@users.noreply.github.com>
2024-06-19 17:13:17 -0300

    aspdsaef

--- js.js
+++ js.js
@@ -1,13 +1,13 @@
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
 function myFunction() {
-    document.getElementsByClassName("myDropdown").classList.toggle("show");
+    document.getElementsByClassName("dropdown-content").classList.toggle("show");
   }
   
   // Close the dropdown if the user clicks outside of it
   window.onclick = function(event) {
     if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var dropdowns = document.getElementsByClassName("myDropdown");
       var i;
       for (i = 0; i < dropdowns.length; i++) {
         var openDropdown = dropdowns[i];
