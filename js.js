e1e3e2bfdaf627093daf8393e2e718030366ecd4
Linxusthuis <140453732+Linxusthuis@users.noreply.github.com>
2024-06-21 09:35:31 -0300

    talvezz funfe

--- js.js
+++ js.js
@@ -4,29 +4,20 @@ function menu() {
 
 // Close the dropdown if the user clicks outside of it
 window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
+  if (!event.target.matches('.dropbtn')||!event.target.matches('.butao')) {
     var dropdowns = document.getElementsByClassName("dropdown-content");
+    var menu = document.getElementsByClassName("menu")
     var i;
-    for (i = 0; i < dropdowns.length; i++) {
+    for (i = 0; i < dropdowns.length || i < menu.length; i++) {
       var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
+      var openmenu = menu[i]
+      if (openDropdown.classList.contains('show') || openmenu.classList.contains('show')) {
         openDropdown.classList.remove('show');
+        openmenu.classList.remove('show');
       }
     }
   }
 }
 function menu2(){
   document.getElementById("menu").classList.add("show")
-}
-window.onclick = function(event) {
-  if (!event.target.matches('.butao')) {
-    var dropdown = document.getElementsByClassName("menu");
-    var i;
-    for (i = 0; i < dropdown.length; i++) {
-      var openDropdown = dropdown[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
 }
