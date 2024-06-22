6aa2759765831a106b6f704d4b1e5d4338040438
Linxusthuis <140453732+Linxusthuis@users.noreply.github.com>
2024-06-22 17:17:34 -0300

    kkkkk

--- js.js
+++ js.js
@@ -1,55 +1,29 @@
 function menu() {
   document.getElementById("myDropdown").classList.add("show");
+  console.log(document.getElementById("myDropdown").classList);
+//  document.getElementById("menu").classList.remove('show');
 }
 
 // Close the dropdown if the user clicks outside of it
  window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')||!event.target.matches('.butao')) {
+  if (!event.target.matches('.dropbtn') && !event.target.matches('.butao') ) {
     var dropdowns = document.getElementsByClassName("dropdown-content");
-    var menu = document.getElementsByClassName("menu")
+    // = document.getElementById("menu")
+    var menu = document.getElementById("menu");
     var i;
-    for (i = 0; i < dropdowns.length || i < menu.length; i++) {
-      var openDropdown = dropdowns[i];
-      var openmenu = menu[i]
-      if (openDropdown.classList.contains('show') || openmenu.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-        openmenu.classList.remove('show');
-      }
-    }
+    for (var i = 0; i < dropdowns.length || i < menu.length; i++) {
+      console.log("então");
+       var openDropdown = dropdowns[i];
+       var openmenu = menu[i];
+       if (dropdowns[i].classList.contains('show') || menu.classList.contains('show')) {
+        console.log(dropdowns.classList)
+        dropdowns[i].classList.remove("show");
+        document.getElementById("menu").classList.remove('show');
+ }
+   }
+ 
   }
-}
-function menu2(){
-  document.getElementById("menu").classList.add("show")
-}
-function menu() {
-  document.getElementById("myDropdown").classList.add("show");
-}
 
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
 function menu2(){
   document.getElementById("menu").classList.add("show")
-}
-window.onclick = function(event) {
-  if (!event.target.matches('.butao')) {
-    var dropdowns = document.getElementsByClassName("menu");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
 }
